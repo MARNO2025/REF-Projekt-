e8b04a2cca7b715bb7b7c475194591b8d39b8de0
MARNO2025 <philipp.arnold@live.de>
2026-01-05 14:41:02 +0100

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-script_path = r"C:\Users\phili\Documents\REF-Projekt\app.py"
-
-# Startet Streamlit in einem neuen CMD-Fenster
-os.system(f'start cmd /k "{sys.executable} -m streamlit run "{script_path}""')
